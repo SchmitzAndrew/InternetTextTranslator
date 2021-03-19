45f3b3b8edf7687ba3450cbc1388d840148e0db8
Andrew Schmitz <47334046+SchmitzAndrew@users.noreply.github.com>
2021-03-18 23:08:49 -0800

    Add files via upload
    manual upload

--- Main.py
+++ Main.py
@@ -4,17 +4,20 @@ from Translator import *
 print("Welcome to Internet Text Translator!")
 print("1 - Shifting Text")
 print("2 - wAckY teXt")
-print("3 - Whiny Text")
-print("4 - Text Spacer")
-print("5 - Text Reducer")
-print("6 - Text Repeater")
+print("3 - Text Reducer")
+print("4 - Text Repeater")
 
 selection = int(input("Enter your desired translation here: "))
 phrase = input("Enter the phrase you would like to translate: ")
+
 if selection == 1:
     shiftText(phrase)
 elif selection == 2:
     wackyText(phrase)
+elif selection ==3:
+    textReducer(phrase)
+elif selection == 4:
+    textRepeater(phrase)
 else:
     print("Not a valid selection.")
 
--- Translator.py
+++ Translator.py
@@ -1,5 +1,6 @@
 import random
 
+#text shifter
 
 def printSpaces(num):
     s = " "
@@ -7,26 +8,27 @@ def printSpaces(num):
 
 
 def shiftText(str):
-    lines = int(input("How many lines: "))
+    shifter_lines = int(input("How many lines: "))
     length = len(str)
     if length <= 2:
         print("Input needs to be longer")
     else:
         spaces = int(length / 2)
         linecount = 0
-        for i in range(int(lines / spaces)):
+        for i in range(int(shifter_lines / spaces)):
             for s in range(spaces):
-                if linecount >= lines:
+                if linecount >= shifter_lines:
                     break
                 print(printSpaces(s % spaces) + str)
                 linecount += 1
             for s in range(spaces):
-                if linecount >= lines:
+                if linecount >= shifter_lines:
                     break
                 print(printSpaces(spaces - s % spaces) + str)
                 linecount += 1
 
 
+#Wacky Switch
 def wackySwitch(t):
     if random.random() < .3:
         t = t.swapcase()
@@ -37,3 +39,36 @@ def wackyText(input):  # cant use str
     wacky_phrase = [wackySwitch(t) for t in phrase]
     result = ''.join([str(x) for x in wacky_phrase])
     print(result)
+
+
+#Text Spacer
+def textSpacer(input):
+    char_length = len(input) - input.count(' ')
+    list_phrase = list(input)
+    list_phrase = [t.strip(' ') for t in list_phrase]
+    list_phrase = [t for t in list_phrase if t]  # removes empty strings
+    line_length = 10
+    chars_per_lines = 3
+    global temp_list
+    temp_list = []
+    for i in range(line_length):
+        temp_list.append(" ")
+
+
+def textReducer(phrase):
+    for i in range(len(phrase)):
+        print(phrase[0:len(phrase)- i])
+
+
+def textRepeater(phrase):
+    lines = int(input("How many lines: "))
+    if lines < 10:
+        for i in range(lines):
+            print(phrase)
+    elif lines > 100: #change this
+        print("Woah there kiddo.")
+    else:
+        f = open("output.txt", "w")
+        for i in range(lines):
+            f.write(phrase + "\n")
+
